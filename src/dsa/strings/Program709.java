b9445206fbce5e941c48175be8f43607e6432cbf
asiff mahmad <asiff@asiffs-MacBook-Air.local>
2025-12-05 18:28:17 +0530

    asiff | added string  problem

---
+++ src/dsa/strings/Program709.java
@@ -0,0 +1,20 @@
+package dsa.strings;
+
+public class Program709 {
+	
+	
+	public static void main(String[] args) {
+		
+		String name = toLowerCase("ASIFF");
+		
+		System.out.println(name);
+		
+	}
+	
+	public static String toLowerCase(String s) {
+
+        return s.toLowerCase();
+        
+    }
+
+}
